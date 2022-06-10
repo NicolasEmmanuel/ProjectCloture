71bc958061e34468948f10ae2f48b9f4abf7cd49
gitCDA <103611339+gitCDA@users.noreply.github.com>
2022-06-10 11:58:23 -0400

    formation avant layout

--- routes/formation.php
+++ routes/formation.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\FormationController;
 /**Route FORMATION */
 Route::get('/formation',[FormationController::class,'index'])->name('formation');
 
+/**Route DÉTAIL FORMATION */
+Route::get('/formation/detail',[FormationController::class,'detail'])->name('formation-detail');
+
 /**Route Admin FORMATION */
 Route::get('/admin/formation',[FormationController::class,'admin'])->middleware('auth')
 ->name('auth-formation');
